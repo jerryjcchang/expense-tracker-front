32cf05f9a1f09b36d503e3f05f64e8bcc4908f26
github username <github email address>
2020-02-26 18:12:41 -0500

    done

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amt, setAmt] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: parseInt(amt)
+    };
+    debugger;
+    addTransaction(newTransaction);
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
@@ -30,7 +43,7 @@ export const AddTransaction = () => {
             id="amount"
             value={amt}
             onChange={e => {
-              setAmt(parseInt(e.target.value));
+              setAmt(e.target.value);
             }}
             placeholder="Enter amount..."
           />
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -9,11 +10,8 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        {transactions.map(transactions => (
-          <li className="minus">
-            {transactions.text} <span>-400</span>
-            <button className="delete-btn">x</button>
-          </li>
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
