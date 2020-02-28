e1d0df0d2c1ed205fdb39a9ed4e7229c3e66d86f
github username <github email address>
2020-02-27 20:12:57 -0500

    backend added

---
+++ client/src/components/Transaction.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const className = sign === "-" ? "minus" : "plus";
+  console.log(transaction);
+
+  return (
+    <li className={transaction.amount > 0 ? "plus" : "minus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}
+        {Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => {
+          deleteTransaction(transaction.id);
+        }}
+      >
+        x
+      </button>
+    </li>
+  );
+};
---
+++ controllers/transactions.js
@@ -0,0 +1,19 @@
+// @desc    Get all transactions
+// @route   /api/v1/transactions
+// @access  Public
+exports.getTransactions = (req, res, next) => {
+  res.send("GET transactions");
+};
+// @desc    Delete transaction
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+// @desc    Delete transaction
+// @route   DELETE /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transactions");
+};
